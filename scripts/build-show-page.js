72514d5c0f1a6af319ad5af42ad98f7e559699c1
Gustavo Costa <gustavo.lcostaa@gmail.com>
2023-02-21 20:37:51 -0800

    sprint-3 first commit

--- scripts/build-show-page.js
+++ scripts/build-show-page.js
@@ -1,90 +1,149 @@
-const shows = [
-  {
-    date: "Mon Sept 06 2021",
+// const shows = [
+//   {
+//     date: "Mon Sept 06 2021",
 
-    venue: "Ronald Lane",
+//     venue: "Ronald Lane",
 
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Tue Sept 21 2021",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Tue Sept 21 2021",
 
-    venue: "Pier 3 East",
+//     venue: "Pier 3 East",
 
-    location: "San Francisco, CA",
-  },
+//     location: "San Francisco, CA",
+//   },
 
-  {
-    date: "Fri Oct 15 2021",
+//   {
+//     date: "Fri Oct 15 2021",
 
-    venue: "View Lounge",
+//     venue: "View Lounge",
 
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Sat Nov 06 2021",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Sat Nov 06 2021",
 
-    venue: "Hyatt Agency",
+//     venue: "Hyatt Agency",
 
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Nov 26 2021",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Fri Nov 26 2021",
 
-    venue: "Moscow Center",
+//     venue: "Moscow Center",
 
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Wed Dec 15 2021",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Wed Dec 15 2021",
 
-    venue: "Press Club",
+//     venue: "Press Club",
 
-    location: "San Francisco, CA",
-  },
-];
+//     location: "San Francisco, CA",
+//   },
+// ];
 
-const show = document.querySelector(".shows");
+// const show = document.querySelector(".shows");
 
-for (let j = 0; j < shows.length; j++) {
-  const showsContainer = document.createElement("div");
-  showsContainer.classList.add("shows__container");
+// for (let j = 0; j < shows.length; j++) {
+//   const showsContainer = document.createElement("div");
+//   showsContainer.classList.add("shows__container");
 
-  const dateTitle = document.createElement("h4");
-  dateTitle.classList.add("shows__header__info--date");
-  dateTitle.innerText = "DATE";
+//   const dateTitle = document.createElement("h4");
+//   dateTitle.classList.add("shows__header__info--date");
+//   dateTitle.innerText = "DATE";
 
-  const showDate = document.createElement("p");
-  showDate.classList.add("shows__header__about__date");
-  showDate.innerText = shows[j].date;
+//   const showDate = document.createElement("p");
+//   showDate.classList.add("shows__header__about__date");
+//   showDate.innerText = shows[j].date;
 
-  const showVenue = document.createElement("h4");
-  showVenue.classList.add("shows__header__info--venue");
-  showVenue.innerText = "VENUE";
+//   const showVenue = document.createElement("h4");
+//   showVenue.classList.add("shows__header__info--venue");
+//   showVenue.innerText = "VENUE";
 
-  const venueTitle = document.createElement("p");
-  venueTitle.classList.add("shows__header__about__venue");
-  venueTitle.innerText = shows[j].venue;
+//   const venueTitle = document.createElement("p");
+//   venueTitle.classList.add("shows__header__about__venue");
+//   venueTitle.innerText = shows[j].venue;
 
-  const showLocation = document.createElement("h4");
-  showLocation.classList.add("shows__header__info--location");
-  showLocation.innerText = "LOCATION";
+//   const showLocation = document.createElement("h4");
+//   showLocation.classList.add("shows__header__info--location");
+//   showLocation.innerText = "LOCATION";
 
-  const locationTitle = document.createElement("p");
-  locationTitle.classList.add("shows__header__about__location");
-  locationTitle.innerText = shows[j].location;
+//   const locationTitle = document.createElement("p");
+//   locationTitle.classList.add("shows__header__about__location");
+//   locationTitle.innerText = shows[j].location;
 
-  const showButton = document.createElement("button");
-  showButton.innerText = "BUY TICKETS";
-  showButton.classList.add("shows__btn");
+//   const showButton = document.createElement("button");
+//   showButton.innerText = "BUY TICKETS";
+//   showButton.classList.add("shows__btn");
 
-  showsContainer.appendChild(dateTitle);
-  showsContainer.appendChild(showVenue);
-  dateTitle.appendChild(showDate);
-  showVenue.appendChild(venueTitle);
-  showsContainer.appendChild(showLocation);
-  showLocation.appendChild(locationTitle);
-  showsContainer.appendChild(showButton);
+//   showsContainer.appendChild(dateTitle);
+//   showsContainer.appendChild(showVenue);
+//   dateTitle.appendChild(showDate);
+//   showVenue.appendChild(venueTitle);
+//   showsContainer.appendChild(showLocation);
+//   showLocation.appendChild(locationTitle);
+//   showsContainer.appendChild(showButton);
 
-  show.appendChild(showsContainer);
+//   show.appendChild(showsContainer);
+// }
+
+const link = "https://project-1-api.herokuapp.com/showdates";
+const apiKey = "?api_key=b70c7207-fea6-4ea5-98db-d101d901be50";
+
+const tableBody = document.querySelector(".table__body");
+
+showTable();
+//function takes data from api response and returns table of schedule of shows on the page
+function showTable() {
+  const getShows = axios.get(link + apiKey);
+  getShows.then((res) => {
+    const showsArray = res.data;
+    tableBody.innerHTML = "";
+    showsArray.forEach((show) => {
+      const tableRow = document.createElement("div");
+      tableRow.classList.add("table__row");
+      tableBody.appendChild(tableRow);
+
+      const tableDate = document.createElement("span");
+      tableDate.classList.add("table__date");
+      tableDate.classList.add("cell");
+      tableDate.setAttribute("data-label", "date");
+
+      tableDate.innerHTML = new Date(Number(show.date)).toDateString();
+      tableRow.appendChild(tableDate);
+
+      const tableVenue = document.createElement("span");
+      tableVenue.setAttribute("data-label", "venue");
+      tableVenue.classList.add("cell");
+      tableVenue.innerHTML = show.place;
+      tableRow.appendChild(tableVenue);
+
+      const tableCity = document.createElement("span");
+      tableCity.setAttribute("data-label", "city");
+      tableCity.classList.add("cell");
+      tableCity.innerHTML = show.location;
+      tableRow.appendChild(tableCity);
+
+      const btnCell = document.createElement("span");
+      btnCell.classList.add("cell");
+      tableRow.appendChild(btnCell);
+
+      const tableBtn = document.createElement("button");
+      tableBtn.classList.add("btn");
+      tableBtn.innerHTML = "buy tickets";
+      btnCell.appendChild(tableBtn);
+    });
+  });
 }
+
+//On click, clicked row is highlighted. removes all other highlights.
+tableBody.addEventListener("click", (event) => {
+  const selectedRow = event.target.parentElement;
+  const allRows = document.querySelectorAll(".table__row");
+  allRows.forEach((row) => {
+    row.classList.remove("table__row--selected");
+  });
+  selectedRow.classList.add("table__row--selected");
+});
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,95 +1,95 @@
-const commentsArr = [
-  {
-    name: "Connor Walton",
-    posted: 1613608320000,
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Emilie Beach",
-    posted: 1610238720000,
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    name: "Miles Acosta",
-    posted: 1608510720000,
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-];
+// const commentsArr = [
+//   {
+//     name: "Connor Walton",
+//     posted: 1613608320000,
+//     comment:
+//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+//   },
+//   {
+//     name: "Emilie Beach",
+//     posted: 1610238720000,
+//     comment:
+//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+//   },
+//   {
+//     name: "Miles Acosta",
+//     posted: 1608510720000,
+//     comment:
+//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//   },
+// ];
 
-const commentSection = document.querySelector(".comment__section");
+// const commentSection = document.querySelector(".comment__section");
 
-const myForm = document.getElementById("form");
+// const myForm = document.getElementById("form");
 
-myForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+// myForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
 
-  const newComment = {
-    name: event.target.name.value,
-    comment: event.target.comment__textarea.value,
-    posted: Date.now(),
-  };
+//   const newComment = {
+//     name: event.target.name.value,
+//     comment: event.target.comment__textarea.value,
+//     posted: Date.now(),
+//   };
 
-  //   console.log(newComment);
-  commentsArr.push(newComment);
-  //   commentsArr.unshift(newComment);
-  myForm.reset();
+//   //   console.log(newComment);
+//   commentsArr.push(newComment);
+//   //   commentsArr.unshift(newComment);
+//   myForm.reset();
 
-  allComments();
-});
+//   allComments();
+// });
 
-function allComments() {
-  commentsArr.sort((a, b) => {
-    return b.posted - a.posted;
-  });
-  commentSection.innerHTML = "";
-  for (let i = 0; i < commentsArr.length; i++) {
-    const commentArticle = document.createElement("article");
+// function allComments() {
+//   commentsArr.sort((a, b) => {
+//     return b.posted - a.posted;
+//   });
+//   commentSection.innerHTML = "";
+//   for (let i = 0; i < commentsArr.length; i++) {
+//     const commentArticle = document.createElement("article");
 
-    commentArticle.classList.add("comment__container");
+//     commentArticle.classList.add("comment__container");
 
-    const commentImg = document.createElement("img");
+//     const commentImg = document.createElement("img");
 
-    commentImg.classList.add("comment__img");
+//     commentImg.classList.add("comment__img");
 
-    commentImg.setAttribute("src", "assets/images/Default_pfp.svg.png");
+//     commentImg.setAttribute("src", "assets/images/Default_pfp.svg.png");
 
-    const commentContainer = document.createElement("div");
+//     const commentContainer = document.createElement("div");
 
-    commentContainer.classList.add("comment__profile__container");
+//     commentContainer.classList.add("comment__profile__container");
 
-    const profileHeader = document.createElement("div");
-    profileHeader.classList.add("comment__profile__header");
+//     const profileHeader = document.createElement("div");
+//     profileHeader.classList.add("comment__profile__header");
 
-    const userName = document.createElement("h4");
+//     const userName = document.createElement("h4");
 
-    userName.innerText = commentsArr[i].name;
+//     userName.innerText = commentsArr[i].name;
 
-    const postedElement = document.createElement("p");
-    postedElement.innerText = new Date(
-      commentsArr[i].posted
-    ).toLocaleDateString();
+//     const postedElement = document.createElement("p");
+//     postedElement.innerText = new Date(
+//       commentsArr[i].posted
+//     ).toLocaleDateString();
 
-    const userComments = document.createElement("p");
-    userComments.innerText = commentsArr[i].comment;
+//     const userComments = document.createElement("p");
+//     userComments.innerText = commentsArr[i].comment;
 
-    commentArticle.appendChild(commentImg);
-    commentArticle.appendChild(commentContainer);
-    commentContainer.appendChild(profileHeader);
-    profileHeader.appendChild(userName);
-    profileHeader.appendChild(postedElement);
-    commentContainer.appendChild(userComments);
-    commentSection.appendChild(commentArticle);
-  }
-}
+//     commentArticle.appendChild(commentImg);
+//     commentArticle.appendChild(commentContainer);
+//     commentContainer.appendChild(profileHeader);
+//     profileHeader.appendChild(userName);
+//     profileHeader.appendChild(postedElement);
+//     commentContainer.appendChild(userComments);
+//     commentSection.appendChild(commentArticle);
+//   }
+// }
 
-allComments();
+// allComments();
 
-const form = document.querySelector("#form");
-const input = document.querySelector("#name");
-const comment = document.querySelector("#comment");
+// const form = document.querySelector("#form");
+// const input = document.querySelector("#name");
+// const comment = document.querySelector("#comment");
 
 // form.addEventListener("submit", submitForm);
 // form.addEventListener("input", validateInput);
@@ -138,3 +138,147 @@ const comment = document.querySelector("#comment");
 //     comment.style.border = "2px solid red";
 //   }
 // }
+
+const link = "https://project-1-api.herokuapp.com/comments";
+const apiKey = "?api_key=b70c7207-fea6-4ea5-98db-d101d901be50";
+//function takes in comment data and return html card with format
+function displayComment(comment) {
+  const commentLi = document.createElement("li");
+  commentLi.classList.add("item");
+  commentsList.appendChild(commentLi);
+
+  const itemImg = document.createElement("img");
+  itemImg.classList.add("item__img");
+  itemImg.setAttribute("src", " ");
+  commentLi.appendChild(itemImg);
+
+  const itemSection = document.createElement("div");
+  itemSection.classList.add("item__section");
+  commentLi.appendChild(itemSection);
+
+  const itemHeader = document.createElement("div");
+  itemHeader.classList.add("item__title");
+  itemSection.appendChild(itemHeader);
+
+  const itemName = document.createElement("p");
+  itemName.classList.add("item__name");
+  itemName.innerHTML = comment.name;
+  itemHeader.appendChild(itemName);
+
+  const itemDate = document.createElement("p");
+  itemDate.classList.add("item__date");
+  itemDate.innerText = new Date(comment.timestamp).toLocaleDateString("en-US");
+  itemHeader.appendChild(itemDate);
+
+  const itemBody = document.createElement("div");
+  itemBody.classList.add("item__body");
+  itemSection.appendChild(itemBody);
+
+  const bodyCopy = document.createElement("p");
+  bodyCopy.innerText = comment.comment;
+  itemBody.appendChild(bodyCopy);
+
+  const itemFooter = document.createElement("div");
+  itemFooter.classList.add("item__footer");
+  itemSection.appendChild(itemFooter);
+
+  const likeImgLink = document.createElement("a");
+  likeImgLink.classList.add("item__actions", "item__actions--like");
+  likeImgLink.setAttribute("data-id", comment.id);
+  // likeImgLink.setAttribute("onclick", "liked(this)");
+  likeImgLink.setAttribute("value", "like");
+  itemFooter.appendChild(likeImgLink);
+
+  const likeImg = document.createElement("img");
+  likeImg.classList.add("like-img");
+  likeImg.setAttribute("src", "./assets/icons/SVG/icon-like.svg");
+  likeImg.setAttribute("data-id", comment.id);
+  likeImg.setAttribute("value", "like");
+  likeImgLink.appendChild(likeImg);
+
+  const likeCounter = document.createElement("p");
+  likeImg.classList.add("like-counter");
+  likeCounter.innerText = comment.likes;
+  likeImgLink.appendChild(likeCounter);
+
+  const deleteLink = document.createElement("a");
+  deleteLink.classList.add("item__actions", "item__actions--delete");
+  deleteLink.setAttribute("data-id", comment.id);
+  deleteLink.innerText = "Delete";
+  // deleteLink.setAttribute("onclick", "deleteComment(this)");
+  deleteLink.setAttribute("value", "delete");
+  itemFooter.appendChild(deleteLink);
+}
+//function returns comments card list from api response data
+function showCommentsList() {
+  const getComments = axios.get(link + apiKey);
+  getComments.then((response) => {
+    const commentsData = response.data;
+    const commentsArray = commentsData.sort(
+      (a, b) => b.timestamp - a.timestamp
+    );
+    commentsList.innerHTML = "";
+    commentsArray.forEach((comment) => {
+      displayComment(comment);
+    });
+  });
+}
+
+const form = document.querySelector(".form");
+const commentsList = document.querySelector(".comments__list");
+showCommentsList();
+//add comment once the submit button is clicked
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const submittedName = event.target.formName;
+  const submittedComment = event.target.formComment;
+  if (formValidation(submittedName, submittedComment)) {
+    const postedComment = {
+      name: submittedName.value,
+      comment: submittedComment.value,
+    };
+    const addComment = axios.post(link + apiKey, postedComment);
+    addComment.then((response) => {
+      event.target.reset();
+      showCommentsList(response);
+    });
+  }
+});
+
+// listens for clicks on comment like and delete button of a comment and takes action accordingly
+const itemActions = document.querySelector(".comments__list");
+
+itemActions.addEventListener("click", (event) => {
+  if (event.target.getAttribute("data-id")) {
+    const userId = event.target.getAttribute("data-id");
+    if (event.target.getAttribute("value") === "like") {
+      const addLike = axios.put(link + "/" + userId + "/like" + apiKey);
+      addLike.then((response) => {
+        showCommentsList(response);
+      });
+    } else {
+      const delComment = axios.delete(link + "/" + userId + apiKey);
+      delComment.then((response) => {
+        showCommentsList(response);
+      });
+    }
+  }
+});
+
+//function check if the text added in the field is valid characters
+function formValidation(nameField, commentField) {
+  const valid = /^[a-zA-Z]/;
+  if (!valid.test(nameField.value)) {
+    nameField.focus();
+    nameField.value = "";
+    nameField.classList.add("form__input--error");
+    return false;
+  } else if (!valid.test(commentField.value)) {
+    commentField.focus();
+    commentField.value = "";
+    commentField.classList.add("form__input--error");
+    return false;
+  } else {
+    return true;
+  }
+}
